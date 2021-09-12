b3344220c8846dd7714c74520ec39b9b2252b2f6
Sarbottam Chatterjee <88260128+import-sarbottam@users.noreply.github.com>
2021-09-12 19:25:08 +0530

    added google recaptcha in signup

--- src/Components/signup.js
+++ src/Components/signup.js
@@ -19,6 +19,7 @@ export const Signup = () => {
       history.push('/')
     }
   });
+  checkUser();
   
   const [loading, setLoading] = useState(false);
   const [human,setHuman] = useState(false);
