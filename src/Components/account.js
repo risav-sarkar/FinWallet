0697bae850558a4d7463b4756a45d1909bd34ea0
Risav Sarkar <risavabir2001@gmail.com>
2021-09-18 18:37:33 +0530

    Frontend made for changing user name

--- src/Components/account.js
+++ src/Components/account.js
@@ -1,22 +1,24 @@
-import Header from "./mainComponents/header.js";
 import { auth } from "./firebase/firebase";
 import Firebase from "firebase";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { useHistory } from "react-router-dom";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen, faTimes, faCheck } from "@fortawesome/free-solid-svg-icons";
 
 export const Account = () => {
-
   const database = Firebase.database();
 
   const history = useHistory();
   const [userID, setUserID] = useState("");
+  const [name, setName] = useState("User");
+  const [editBtn, setEditBtn] = useState(0);
+  const editName = useRef(name);
+
   const checkUser = auth.onAuthStateChanged((user) => {
     if (user) {
       setUserID(user.uid);
-    }
-    else
-      history.push('/signin')
+      setName(user.displayName);
+    } else history.push("/signin");
   });
   checkUser();
 
@@ -24,16 +26,67 @@ export const Account = () => {
     e.preventDefault();
     // eslint-disable-next-line
     const dataRef = database.ref(userID).remove();
-    history.push('/')
-  }
+    history.push("/");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //Sarbottam
+    console.log(editName.current.value);
+    setEditBtn(0);
+  };
+
   return (
     <main>
       <div className="mainContainer">
-        <Header />
+        <div className="headerAccounts">
+          {editBtn === 0 ? (
+            <h1>
+              {`Hi ${name}`}
+              <button
+                className="editBtn"
+                onClick={() => {
+                  setEditBtn(1);
+                }}
+              >
+                <FontAwesomeIcon icon={faPen} />
+              </button>
+            </h1>
+          ) : (
+            <h1>
+              <form onSubmit={handleSubmit}>
+                <input
+                  className="accountInput"
+                  type="text"
+                  ref={editName}
+                  placeholder={name}
+                  required
+                />
+                <button className="editBtn" type="submit">
+                  <FontAwesomeIcon icon={faCheck} />
+                </button>
+                <button
+                  className="editBtn"
+                  onClick={() => {
+                    setEditBtn(0);
+                  }}
+                >
+                  <FontAwesomeIcon icon={faTimes} />
+                </button>
+              </form>
+            </h1>
+          )}
+        </div>
         <div className="accountContainer">
           <div className="btnContainer">
-            <button onClick={()=>{history.push('/forgot')}}>Change Password</button>
-            <button onClick={(e)=>handleRemove(e)}>Reset All Data</button>
+            <button
+              onClick={() => {
+                history.push("/forgot");
+              }}
+            >
+              Change Password
+            </button>
+            <button onClick={(e) => handleRemove(e)}>Reset All Data</button>
           </div>
         </div>
       </div>
