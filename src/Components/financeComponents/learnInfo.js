c605857d34a06525062f9fc30f9fc15760306562
Risav Sarkar <risavabir2001@gmail.com>
2021-09-20 09:53:16 +0530

    Update learnInfo.js

--- src/Components/financeComponents/learnInfo.js
+++ src/Components/financeComponents/learnInfo.js
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
 
 const LearnInfo = ({ name, desc, pros, cons }) => {
+  let f = 0;
   return (
     <div className="components">
       <div className="headerComp">
@@ -13,7 +14,7 @@ const LearnInfo = ({ name, desc, pros, cons }) => {
           <h3>Pros</h3>
           {pros.map((e) => {
             return (
-              <div className="grid">
+              <div className="grid" key={`${name}${f++}`}>
                 <FontAwesomeIcon className="pros" icon={faPlus} />
                 <p>{e}</p>
               </div>
@@ -24,7 +25,7 @@ const LearnInfo = ({ name, desc, pros, cons }) => {
           <h3>Cons</h3>
           {cons.map((e) => {
             return (
-              <div className="grid">
+              <div className="grid" key={`${name}${f++}`}>
                 <FontAwesomeIcon className="cons" icon={faMinus} />
                 <p>{e}</p>
               </div>
